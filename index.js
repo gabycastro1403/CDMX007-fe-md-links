d2454a3635c21450e5f628c549be2dc8f509df0f
Ed <ed@MacBook-Pro-de-Ed.local>
2019-04-10 15:35:57 -0500

    Subiendo return en la funcion de httpGet y se implementa el comando --validate

--- index.js
+++ index.js
@@ -9,22 +9,24 @@ const validate = require('./validate')
 const colors = require('colors');
 const path = require('path');
 const fs = require('fs');
-//Funcion de ruta de archivo y obtiene el contenido del mismo
+const pathInput = process.argv[2];
 
-const pathInput = process.argv[2] 
-//console.log(process.argv);
-
-if(path.extname(pathInput) === ".md"){
+if (process.argv[3] == '--validate'){
+  fileManager.readFile(pathInput, (data)=>{
+    const linksMd = searchLinks.searchLinks(data)
+    httpGet.getHttp(linksMd)
+    .then(resp => {
+      console.log(validate.links(resp,pathInput))})
+  });
+}else if(path.extname(pathInput) === ".md"){
 fileManager.readFile(pathInput, (data)=>{
   //console.log(data);
-  const linksMd = searchLinks.searchLinks(data);
-  // console.log(linksMd);
-  const validateLink = httpGet.getHttp(linksMd);
-  console.log(validateLink);
-  // const validate2 = validate.validate(validateLink);
-  // console.log(validate2);
+  const linksMd = searchLinks.searchLinks(data)
+  console.log(linksMd);
+  console.log(process.argv);
 })
 } else if(fs.lstatSync(pathInput).isDirectory()){
+
 //Funcion de ruta de directorio y obtiene el contenido y los links del mismo   
 findDir.readdir(pathInput,(list)=>{
   
@@ -32,11 +34,11 @@ findDir.readdir(pathInput,(list)=>{
   //console.log(list);
   result.forEach(element => {
   fileManager.readFile(element, (data) => {
-      const linksMd = searchLinks.searchLinks(data)
-       console.log(linksMd);
-      // const validateLink= httpGet.getHttp(linksMd)
-      // const validate2 = validate.validate(validateLink);
-        })
- }); 
+    const linksMd = searchLinks.searchLinks(data)
+    console.log(linksMd);
+       
+  })
+ }) 
 });
 }
+
